753bdc0018420b62b4942512d526a0b2f211124b
lumanaa <lssuwal@gmail.com>
2024-10-01 14:28:28 +0545

    basic layouts initial

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Background from "./components/Background";
+import Footer from "./sections/Footer";
+import Navbar from "./sections/Navbar";
+import Wrapper from "./sections/Wrapper";
+import './scss/index.scss';
+
+function App() {
+  return (
+    <div className='main-container'>
+      <Background/>
+      <div className='app'>
+        <Navbar />
+        <Wrapper />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
